61bbe22e862112677a1b50866ff5fc6592015df5
ThanhLNP <52529251+ThanhLNP@users.noreply.github.com>
2019-09-23 09:45:48 +0700

    Add database first

---
+++ MVC/EFDatabaseFirst/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EFDatabaseFirst.Models;
+
+namespace EFDatabaseFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly EmployeeContext _dbcontext;
+
+        public HomeController(EmployeeContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var _emplst = _dbcontext.tblEmployees.
+                          Join(_dbcontext.tblSkills, e => e.SkillID, s => s.SkillID,
+                          (e, s) => new EmployeeViewModel
+                          {
+                              EmployeeID = e.EmployeeID,
+                              EmployeeName = e.EmployeeName,
+                              PhoneNumber = e.PhoneNumber,
+                              Skill = s.Title,
+                              YearsExperience = e.YearsExperience
+                          }).ToList();
+            IList<EmployeeViewModel> emplst = _emplst;
+            return View(emplst);
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ MVC/EFDatabaseFirst/Models/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EFDatabaseFirst.Models;
+
+namespace EFDatabaseFirst.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<tblSkill> tblSkills { get; set; }
+        public DbSet<tblEmployee> tblEmployees { get; set; }
+        public DbSet<EFDatabaseFirst.Models.EmployeeViewModel> EmployeeViewModel { get; set; }
+    }
+}

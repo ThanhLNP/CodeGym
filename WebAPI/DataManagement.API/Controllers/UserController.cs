8ae2581e001ee710a8366b24f07bb9d4187b77dd
ThanhLNP <52529251+ThanhLNP@users.noreply.github.com>
2019-09-28 23:34:54 +0700

    Add solution WebAPI

---
+++ WebAPI/DataManagement.API/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using DataManagement.Business.Interfaces;
+using DataManagement.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DataManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        IUserManager _userManager;
+
+        public UserController(IUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _userManager.GetAllUser();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return _userManager.GetUserById(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+            _userManager.AddUser(user);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User user)
+        {
+            _userManager.UpdateUser(user);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _userManager.DeleteUser(id);
+        }
+    }
+}
